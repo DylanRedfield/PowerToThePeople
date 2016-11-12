f6cb659809bd4121db22d89b34d5bc01b29af728
dylanredfield <dylanredfield0938@gmail.com>
2016-11-12 09:22:04 -0500

    first commit

---
+++ PowerVoting/app/src/main/java/com/powervoting/powervoting/MainActivity.java
@@ -0,0 +1,27 @@
+package com.powervoting.powervoting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.FileReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase firebase = FirebaseDatabase.getInstance();
+
+        DatabaseReference ref = firebase.getReference();
+
+        ref.child("test").setValue(null);
+    }
+}
